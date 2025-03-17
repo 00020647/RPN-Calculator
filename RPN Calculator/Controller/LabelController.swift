20943431cd11c9bf74bbe61048dfa3e15ffbb41a
Sam <sbekmuratov2@students.wiut.uz>
2025-03-17 19:46:58 +0500

    17 March

---
+++ RPN Calculator/Controller/LabelController.swift
@@ -0,0 +1,20 @@
+//
+//  LabelController.swift
+//  RPN Calculator
+//
+//  Created by Sukhrob on 17/03/25.
+//
+
+import UIKit
+
+class LabelController: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
